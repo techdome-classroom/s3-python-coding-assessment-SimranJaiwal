f137b9878ff47c2b5bf74f1ae414f20ea40b442b
SimranJaiwal <161817125+SimranJaiwal@users.noreply.github.com>
2024-10-07 15:32:29 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -6,5 +6,30 @@ class Solution(object):
         """
         pass
 
+roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        total = 0  # Total value of the Roman numeral
+        prev_value = 0  # Previous numeral value (initially 0)
+
+        # Iterate over the string in reverse
+        for char in reversed(s):
+            curr_value = roman_map[char]  # Get the current numeral value
+
+            # If the current value is less than the previous value, subtract it
+            if curr_value < prev_value:
+                total -= curr_value
+            else:
+                total += curr_value  # Otherwise, add it to the total
+
+            prev_value = curr_value  # Update previous value for next iteration
+        
+        return total
 
